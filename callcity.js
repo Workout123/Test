fa130d36e3148f4351488b5985e54585d2f0aed9
subha <subhasree.g@infibeam.net>
2016-12-08 17:41:31 +0530

    check in CityDescp,callcity file

---
+++ callcity.js
@@ -0,0 +1,21 @@
+function givedetailsParis()
+{
+ //var cityChoosed=document.getElementsByName("cityname");
+// var i;
+// for(i=0;i<cityChoosed.length;i++)
+// {
+//	if(cityChoosed[i].id=="paris")
+//	{
+		document.getElementById("part2").innerHTML="Paris is a major european city and a gloabel centere for art,fashion and culture.";
+//	}
+//	else if (cityChoosed[i].id=="germany")
+//	{
+//		document.getElementById("part2").innerHTML="germany is western european city with landscape of forests,north sea beaches.";
+//	}
+
+// }
+}
+function givedetailsGermany()
+{
+	document.getElementById("part2").innerHTML="germany is western european city with landscape of forests,north sea beaches.";
+}
