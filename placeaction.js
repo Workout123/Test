773f68aa5c31f431a42d3ddf7019e7016fc92b76
VDharshini <v.dharshini@infibeam.net>
2016-12-12 12:48:58 +0530

    place.html added

--- placeaction.js
+++ placeaction.js
@@ -1,31 +1,29 @@
 (function(){
-var places=["bangalore","chennai","Hyderabad"];
+var places=["Bangalore","Chennai","Hyderabad"];
 var description=["Bangalore, officially known as Bengaluru is the capital of the Indian state of Karnataka..","Chennai, formerly known as Madras is the capital of the Indian state of Tamil Nadu..","Hyderabad is the capital of the southern Indian state of Telangana.."];
 
 function show_description(i)
 {
- return function(){ document.getElementById("print").innerHTML=description[i];};
+  function inner_print()
+  {
+     document.getElementById("description").innerHTML=description[i];
+  }
+  return inner_print;
 }
 
 function create_button()
 {
-for(var i=0;i<places.length;i++)
-{
-var button = document.createElement("button");
-button.setAttribute("class","button");
-button.innerHTML = places[i];
-var body = document.getElementById("body");
-body.appendChild(button);
-var linebreak = document.createElement("br");
-body.appendChild(linebreak);
-button.addEventListener("click",show_description(i));
+  for(var i=0;i<places.length;i++)
+  {
+    var button = document.createElement("button");
+    button.setAttribute("class","button");
+    button.innerHTML = places[i];
+    var body=document.getElementById("button_column"); 
+    body.appendChild(button);
+    var linebreak = document.createElement("br");
+    body.appendChild(linebreak);
+    button.addEventListener("click",show_description(i));
+  }
 }
-}
- create_button();
-
-var fileref=document.createElement("link");
-        fileref.setAttribute("rel", "stylesheet");
-        fileref.setAttribute("type", "text/css");
-        fileref.setAttribute("href", "buttonstyle.css");
-}
-)();
+create_button();
+})();
