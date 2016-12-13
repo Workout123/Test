aa02d3c162b36aec6d26594a53fd0827181a0ba1
subha <subhasree.g@infibeam.net>
2016-12-13 12:28:15 +0530

    css position change made

--- city2js.js
+++ city2js.js
@@ -2,45 +2,66 @@
 function()
 {
 	var clist={};
-	clist["london"] = "Welcome to Visit London , your official city guide to London, England. Find things to do in London, days out in London, London attractions and sightseeing, what's on, London events, theatre, tours, restaurants and hotels in London. Plan your trip to London with useful traveller information.";
-	clist["paris"]  = "Every year as summer draws to a close, Paris museums and galleries come up with new ways for visitors to experience art and history and explore our changing planet. This autumn a strong line-up of fascinating exhibitions, staged in over 100 museums, invites us to discover the rich diversity of the world around us.";
-	clist["germay"] = "As Europe's largest economy and second most populous nation (after Russia), Germany is a key member of the continent's economic, political, and defense organizations. ";
+	clist["london"] = {des: "Welcome to Visit London , your official city guide to London, England. Find things to do in London, days out in London, London attractions and sightseeing, what's on, London events, theatre, tours, restaurants and hotels in London. Plan your trip to London with useful traveller information.",
+			   x  : 100,
+			   y  : 150
+			  };
+	clist["paris"]  = {des: "Every year as summer draws to a close, Paris museums and galleries come up with new ways for visitors to experience art and history and explore our changing planet. This autumn a strong line-up of fascinating exhibitions, staged in over 100 museums, invites us to discover the rich diversity of the world around us.",
+			   x  : 110,
+			   y  : 200
+			  };	
+	clist["germay"] = {des: "As Europe's largest economy and second most populous nation (after Russia), Germany is a key member of the continent's economic, political, and defense organizations. ",
+			   x  :  120,
+			   y  :  250
+			  };
 
-	function get(k)
+	function getdes(k)
 	{
-		return clist[k];
+		return clist[k].des;
+	}
+	function getx(k)
+	{
+		return clist[k].x;
+	}
+	function gety(k)
+	{	
+		return clist[k].y;
 	}
 
 	var nme=document.getElementById("nme");
 	var names=Object.keys(clist);
 	var body=document.getElementsByTagName("body")[0];
+
 	for(var i in names)
 	{
 		var b=document.createElement("BUTTON");
 		var t=document.createTextNode(names[i]);
 		b.appendChild(t);
 
-		b.setAttribute("name","cty");
-		b.setAttribute("id",names[i]);
-
 		nme.appendChild(b);
 		var brk=document.createElement("br");
 		nme.appendChild(brk);
 		var brk2=document.createElement("br");
 		nme.appendChild(brk2);		
-
-		b.addEventListener("click",function(){displayDescp(this);},false);
+//alert(""+names[i]);
+		b.addEventListener("click",create_handler(names[i]));
+//b.addEventListener("click",function(){alert("names : "+i);document.getElementById("puthere").innerHTML=get(names[i])});
 
 	}
 
-
-	function displayDescp(e)
-	{
-//alert("buton clicked: "+e.id);
-		document.getElementById("puthere").innerHTML=get(e.id);
+	function create_handler(id) {
+//		alert("in handler : "+id);
+		return function()
+			{
+//				alert("inner func: "+id+ " x:  "+getx(id)+"  y: "+gety(id));
+				document.getElementById("docss").style.position="absolute";
+				document.getElementById("docss").style.top=getx(id)+"px";
+				document.getElementById("docss").style.right=gety(id)+"px";
+ 				document.getElementById("docss").innerHTML=getdes(id);
+			};
+//		alert("after dec in context of outer");
 	}
-
-//	nme.addEventListener("click",displayDescp(this),false);
 
 }
 )();
+	
