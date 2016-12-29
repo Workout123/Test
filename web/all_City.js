8fd43a3966fd5a0e0d2f46014d11c6695d3bf629
rvaish20 <vaishali.r@infibeam.net>
2016-12-29 18:53:27 +0530

    taking file from web server

---
+++ web/all_City.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  var wrapl = document.getElementById("leftside");
+  var br = document.createElement("br");
+  var x1 = document.createElement("BUTTON");
+  var t1 = document.createTextNode("refresh");
+  x1.appendChild(t1);
+  x1.addEventListener('click',refresh);
+  document.body.appendChild(x1);
+    
+  var drop = document.createElement("select");
+  drop.setAttribute("id","city");
+  getJson();
+    
+  function getJson(){
+    $.getJSON('http://localhost/test/allCity.json', function (data) {
+      wrapl.append(drop);
+      var op = new Option();
+      op.value = "";
+      op.text = "select city";
+      drop.options.add(op);
+      for(var i in data.items){
+        var op = new Option();
+        op.value = data.items[i];
+        op.text = data.items[i];
+        drop.options.add(op);
+      }
+      
+      $("#city").on("change",function(){
+        var path = "http://localhost/test/"+document.getElementById("city").value+".json";
+        console.log(path);
+        $.getJSON(path, function (data) {
+          console.log(" success");
+          document.getElementById("cit").innerHTML = data.desc;
+        });
+      });
+    });
+  }
+
+  function refresh(){
+    $('#city option[value!="0"]').remove();
+    $("#city").remove();
+    getJson();
+  }
+});
+
+
+
+
