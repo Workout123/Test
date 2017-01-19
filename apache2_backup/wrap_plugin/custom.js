d73cd31aebe92608c85fc9b9764620d1861b831c
subha <subhasree.g@infibeam.net>
2017-01-19 17:50:52 +0530

    wrapping jquery plugin inside angular directive

--- apache2_backup/wrap_plugin/custom.js
+++ apache2_backup/wrap_plugin/custom.js
@@ -1,6 +1,97 @@
 var app=angular.module('ImgSlider',[]);
+//img slider in directive with img sources passed
+app.directive("imgslidedynamic",function($timeout)
+	{
+	return{
+			restrict:'EA',
+			
+  			scope:{
+  					imgsrc: "=imgs"
+  			},
+
+  			template: 	'<div class="rslides">'+
+  						  '<div ng-repeat="everyimg in imgsrc">'+
+  						  	'<img ng-src="{{everyimg}}">'+
+  						  '</div>'+
+ 	 					'</div>'
+			,			
+			link:	function(scope,element)
+					{
+
+						console.log("in directive");
+
+						//$(window).on('load',func) // will work
+						//$timeout // wil work
+
+						 //angular.element.ready( //doesnot work
+						 //scope.$on('$includeContentLoaded',// doesnot work
+
+						 element.ready(function(){
+
+						 		//angular.element(element[0].querySelector('.rslides')).responsiveSlides(); // will also work
+								
+								// var wrappedQueryResult = angular.element('.rslides');
+								// console.log("wr: ",wrappedQueryResult);						
+								// wrappedQueryResult.responsiveSlides();   								
+
+								var queryResult=element[0].querySelector('.rslides');
+								var wrappedQueryResult = angular.element(queryResult);
+								wrappedQueryResult.responsiveSlides();
+								console.log("wr : ",wrappedQueryResult)
+								
+        				});	
+						
+            		} 
+            		/*compile: function(tElem, tAttrs){
+            			console.log("in compile");
+            			return {
+            				post: function(scope,element){
+            					console.log("ele",element);
+            					$(window).ready(function(){
+
+						 		//angular.element(element[0].querySelector('.rslides')).responsiveSlides(); // will also work
+								
+								var wrappedQueryResult = angular.element('.rslides');
+								console.log("wr: ",wrappedQueryResult);						
+								wrappedQueryResult.responsiveSlides();   								
+								
+        						});	
+            				}
+            			}
+            		}*/
+
+        };
+	});
+
+var controllers={};
+app.service('ImgService',function($http,$q){
+  		this.findMe= function(url)
+ 					{ 	 	
+ 						var def=$q.defer();	
+ 						var data=$http.get(url);
+ 						console.log("data in serv: "+data);
+
+ 						def.resolve(data); 						
+ 						return def.promise; 													
+					}							
+		
+ } ); 
+
+
+controllers.ImgCtrl= function($scope,ImgService,$q)
+						{
+							console.log("in controller");
+							ImgService.findMe("http://localhost/wrap_plugin/imagelist.json").then
+								(function(response){
+									$scope.imgarray=response.data.imgsrc;
+								
+									}
+								);							
+
+						}
+app.controller(controllers);
 //img slider in directive without passing img srces
-app.directive("imgslide",function()
+/*app.directive("imgslide",function()
 	{
 
 	return{
@@ -34,73 +125,4 @@ app.directive("imgslide",function()
          			
          		};
 	});
-
-//img slider in directive with img sources passed
-app.directive("imgslidedynamic",function($timeout)
-	{
-
-	return{
-
-			restrict:'EA',
-			
-  			scope:{
-  					imgsrc: "=imgs"
-  			},
-
-  			template: 	'<div class="rslides">'+
-  						  '<div ng-repeat="everyimg in imgsrc">'+
-  						  	'<img ng-src="{{everyimg}}">'+
-  						  '</div>'+
- 	 					'</div>'
-			,
-
-			link: 
-				function(scope,element)
-					{
-						$timeout(function () {
-								console.log("img :"+scope.imgsrc.length);
-
-								var queryResult = element[0].querySelector('.rslides');
-
-								console.log("qr: "+queryResult);
-
-								var wrappedQueryResult = angular.element(queryResult);
-
-								console.log("wr: "+wrappedQueryResult);
-						
-								wrappedQueryResult.responsiveSlides();  
-
-           				
-        				});	
-						
-            		}
-         			
-        };
-	});
-
-var controllers={};
-app.service('ImgService',function($http,$q){
-  		this.findMe= function(url)
- 					{ 	 	
- 						var def=$q.defer();	
- 						var data=$http.get(url);
- 						console.log("data in serv: "+data);
-
- 						def.resolve(data); 						
- 						return def.promise; 													
-					}							
-		
- } ); 
-
-
-controllers.ImgCtrl= function($scope,ImgService,$q)
-						{
-							ImgService.findMe("http://localhost/wrap_plugin/imagelist.json").then
-								(function(response){
-									$scope.imgarray=response.data.imgsrc;
-								
-									}
-								);							
-
-						}
-app.controller(controllers);
+*/
