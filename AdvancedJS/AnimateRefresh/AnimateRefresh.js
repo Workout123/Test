45f0d47a73d0678a75f122145d79c974bce9fc11
Error-403 <94priyanka.a@gmail.com>
2017-02-03 15:13:51 +0530

    Animation with refresh

---
+++ AdvancedJS/AnimateRefresh/AnimateRefresh.js
@@ -0,0 +1,42 @@
+function DoEverything()
+{
+  var pos = 0;
+
+  var MoveText = document.getElementById("TextToMove");
+  MoveText.style.position="relative";
+  MoveText.style.left='0px'
+
+  function Animate()
+  {
+    //console.log(val);
+    pos++;
+    MoveText.style.left=pos+'px';
+  }
+  return {
+    CallAnimate: function() {
+      //console.log("in here");
+      Animate();
+    },
+    CallRefresh: function() {
+      //console.log("refreshing..");
+      pos=0;
+      Animate();
+    }
+
+  }
+}
+
+function SetMotion()
+{
+  Motion = DoEverything();
+  //if(flag==0)
+  setInterval(Motion.CallAnimate, 5);
+  //Motion.CallRefresh();
+  //return Motion;
+}
+
+function SetStop()
+{
+  //var isMotion=SetMotion(1);
+  Motion.CallRefresh();
+}
