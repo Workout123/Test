685a119b920523de0634b55003416265fdc576d2
Error-403 <94priyanka.a@gmail.com>
2017-02-03 15:41:47 +0530

    JumpByX

--- AdvancedJS/Animate2.1/Animate2.1.js
+++ AdvancedJS/Animate2.1/Animate2.1.js
@@ -1,58 +1,43 @@
-var MoveText=null, animate;
-var getval, flag=0, i=1;
-var JumpByx;
-
-var list=new Array(1);
-
-function NewElement()
+function DoEverything()
 {
-  var start=performance.now();
-  for(j=0; j<200; j++)
-    doc.innerHTML+=list.push(i++);
-  doc.innerHTML+="\n";
-  var stop=performance.now();
-  return stop-start;
-  //clearTimeOut(AnimateText);
-}
-//initial values
-function initialize()
-{
-  MoveText = document.getElementById('RollOverText');
-  MoveText.style.position= 'relative';
-  MoveText.style.left = '0px';
-}
+  var pos = 0;
+  var list=new Array(1);
+  var MoveText = document.getElementById("TextToMove");
+  MoveText.style.position="relative";
+  MoveText.style.left='0px'
 
-function MoveRight(x)
-{
-  MoveText.style.left = parseInt(MoveText.style.left) + x + 'px';
-}
+  function Animate()
+  {
+    //console.log(val);
+    //pos++;
+    NewElement();
+    MoveText.style.left=pos+'px';
+  }
 
-/*function MoveLeft()
-{
-  MoveText.style.left = parseInt(MoveText.style.left) - 10 + 'px';
-}*/
+  function NewElement()
+  {
+    var start=performance.now();
+    for(var j=0; j<200; j++)
+      console.log(list.push(j++));
+    var stop=performance.now();
+    pos+=stop-start;
+    //console.log(JumpByX);
+  }
+  return {
+    CallAnimate: function() {
+      //console.log("in here");
+      //NewElement();
+      Animate();
+    }
 
-function AnimateText()
-{
-  getval = parseInt(MoveText.style.left);
-  //setInterval(AnimateText, 100);
-  JumpByx=NewElement();
-  //NewElement();
-  //Boundary conditions
-  //if(getval<=1200)
-    MoveRight(JumpByx);
-
-  //else
-  //{
-    /*MoveLeft();
-    flag=1;
-    if(getval<0)
-      flag=0;*/
-      //initialize();
-  //}
-  //animate=setInterval(AnimateText, timetowait);
-  animate=setInterval(AnimateText, 70);
-  //document.write(MoveText.style.left);
+  }
 }
 
-window.onload=initialize;
+function SetMotion()
+{
+  Motion = DoEverything();
+  //if(flag==0)
+  setInterval(Motion.CallAnimate, 5);
+  //Motion.CallRefresh();
+  //return Motion;
+}
